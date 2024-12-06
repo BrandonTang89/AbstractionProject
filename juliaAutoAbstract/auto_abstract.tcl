2b0c568873584e14b83246e5c21b62ecf55c2354
JuliaScythe <julia.irvine@balliol.ox.ac.uk>
2024-12-06 14:37:13 +0000

    final push before meeting

--- juliaAutoAbstract/auto_abstract.tcl
+++ juliaAutoAbstract/auto_abstract.tcl
@@ -12,7 +12,7 @@ proc freevars_signal {sig} {
 # Returns true if a is a subset (possibly equality) of b
 proc is_subset {a b} {
     foreach x $a {
-        if {[lsearch $b x] == -1} {
+        if {[lsearch $b $x] == -1} {
             return 0
         }
     }
--- juliaAutoAbstract/load.tcl
+++ juliaAutoAbstract/load.tcl
@@ -9,6 +9,9 @@ reset -none
 source ../CommonUtils_Brandon/symsim_utils.tcl
 namespace import symsim::*
 
+
 source auto_abstract_simple.tcl
 
 puts [PR [simple_bp y6 [VAR x_0] [NOT [VAR x_0]]]]
+
+puts [PR_bp [advanced bp [list] y6 [VAR x_0] [NOT [VAR x_0]] base]]
