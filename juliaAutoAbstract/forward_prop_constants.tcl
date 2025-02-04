bda80d11833e364fa376756f00f1bad111d41c16
JuliaScythe <julia.irvine@balliol.ox.ac.uk>
2025-02-04 13:41:56 +0000

    Add symbolic constants to the mux_full case

---
+++ juliaAutoAbstract/forward_prop_constants.tcl
@@ -0,0 +1,44 @@
+# facilites for creating a list of signals that depend entirely on some other set
+
+# https://stackoverflow.com/a/72614138 to make this sourceable from outside this directory
+variable baseDir [file dirname [file normalize [info script]]]
+source [file join $baseDir auto_abstract.tcl]
+
+proc forward_prop {constants} {
+    set result $constants
+
+    # Stores if we're done; any operation that updates $result in this proc must set it true
+    set changed true
+
+    while {$changed} {
+        set changed false
+
+        # TODO possible optimisation excluding 'dead' wires
+        foreach in_sig $result {
+            foreach out_sig [check_symsim -model -get_sig_fanout $in_sig] {
+                if {$out_sig in $result} {
+                    continue
+                }
+
+                if [is_subset [check_symsim -model -get_sig_fanin $out_sig] $result] {
+                    set changed true
+                    set result [list_union $result [list $out_sig]]
+                }
+            } 
+        }
+    }
+
+    return $result
+}
+
+# returns if a bdd depends only on values in the constants list, or if a signal is in the constants list
+proc is_const {constants bdd} {
+    if {$bdd in $constants} {
+        return 1
+    }
+
+    # note that we don't need to do this transitively -- once we get the signals, anything else is handled by
+    # the forward propagation that's already been done.
+    set freevars [check_symsim -expression -depends $bdd]
+    return [is_subset $freevars $constants]
+}
--- juliaAutoAbstract/simulate.tcl
+++ juliaAutoAbstract/simulate.tcl
@@ -4,6 +4,7 @@
 variable baseDir [file dirname [file normalize [info script]]]
 source [file join $baseDir auto_abstract.tcl]
 source [file join $baseDir names.tcl]
+source [file join $baseDir forward_prop_constants.tcl]
 
 
 proc simulate_unit {sig} {
@@ -57,7 +58,7 @@ proc transitive_simulate {bdd} {
         set inclusion [expr {$bdd in [check_symsim -model [check_symsim -model -get] -list input]}]
         set fanin_size [llength [check_symsim -model -get_sig_fanin $bdd]]
         if {$inclusion || $fanin_size == 0} {
-            return [VAR c_$bdd]
+            return [VAR $bdd]
         }
 
         set bdd [simulate_unit $bdd]
@@ -176,7 +177,7 @@ proc bdd_mux_one_type {bdd} {
 # but the gain is tiny and in a large circuit could drastically increase memory consumption since bdd nodes could never be freed
 #
 # the size of the output depends on the BDD ordering -- perhaps it's possible to find orderings that give maximal size somehow?
-proc find_big_ands {bdd needsinvert} {
+proc find_big_ands {bdd needsinvert constants} {
 
     set inputs [TC $bdd]
     set var [lindex $inputs 0]
@@ -193,7 +194,7 @@ proc find_big_ands {bdd needsinvert} {
                 # if the node is an input, terminate the process
                 return [list [list $var $needsinvert]]
             } else {
-                return [find_big_ands [simulate_unit $var] $needsinvert]
+                return [find_big_ands [simulate_unit $var] $needsinvert $constants]
             }
         } else {
             puts "returned early $bdd [bdd_mux_type $bdd]"
@@ -219,13 +220,13 @@ proc find_big_ands {bdd needsinvert} {
     }
 
     # okay, this is an and gate that is consistent with it's ancestors, so we can recurse down it's inputs
-    if {[is_VAR $var]} { 
+    if {[is_VAR $var] || $var in $constants} { 
         # stop recursing, since this is a variable
         set sw_and_list [list [list $var $needsinvert]]
     } else {
-        set sw_and_list [find_big_ands [simulate_unit $var] $invert_sw]
+        set sw_and_list [find_big_ands [simulate_unit $var] $invert_sw $constants]
     }
-    set in_and_list [find_big_ands $sigIn $invert_in]
+    set in_and_list [find_big_ands $sigIn $invert_in $constants]
 
     puts "normal $sw_and_list $in_and_list"
     return [list_union $sw_and_list $in_and_list]
@@ -244,31 +245,60 @@ proc lpop listVar {
 
 # performs the abstraction step on a BDD tree
 # returns an _abstraction list_ of triples (node, high, low)
-proc bdd_mux_abstract {bdd high low name {constants [list]}} {
+proc bdd_mux_abstract {bdd high low name {constants ""}} {
     set inputs [TC $bdd]
 
     set var [lindex $inputs 0]
     set sigHigh [lindex $inputs 1]
     set sigLow [lindex $inputs 2]
 
+    if {[is_const $constants $bdd]} {
+        puts "a>>>>>>>>>>>>>>> $bdd <<<<<<<<<<<<<<<<<< $constants"
+        set t [transitive_simulate $bdd]
+        return [list [list $t $high $low]]
+    }
+
     set mux_type [bdd_mux_type $bdd]
 
     if {$mux_type == "mux_wire"} {
         # then pass everything through to the switching signal
-        return [bdd_abstract $var $high $low $name]
+        return [bdd_abstract $var $high $low $name $constants]
     } elseif {$mux_type == "mux_invert"} {
-        return [bdd_abstract $var $low $high $name]
+        return [bdd_abstract $var $low $high $name $constants]
     } elseif {$mux_type == "mux_const"} {
         error "mux_const hit! this shouldn't happen if the bdd is reduced and it's unclear what to do here"
     } elseif {$mux_type == "mux_full"} {
         set x_name [lindex [make_unique_names $name 1] 0]
         set x [VAR $x_name]
-        set var_ab [bdd_abstract $var [AND [OR $high $low] $x] [AND [OR $high $low] [NOT $x]] $x_name]
-        set sigHigh_ab [bdd_mux_abstract $sigHigh [AND $x $high] [AND $x $low] $x_name]
-        set sigLow_ab [bdd_mux_abstract $sigLow [AND [NOT $x] $high] [AND [NOT $x] $low] $x_name]
 
-        # return [AND [IMPL $var_ab $sigHigh_ab] [IMPL [NOT $var_ab] $sigHigh_ab]]
+        # handle constants on the switching input
+        if {[is_const $constants $var]} {
+            set $x [transitive_simulate $var]
+            set var_ab ""
+        } else {
+            set var_ab [bdd_abstract $var [AND [OR $high $low] $x] [AND [OR $high $low] [NOT $x]] $x_name $constants]
+        }
+
+        # due to the variable ordering condition, it's impossible for these to be constants when sig is not
+        # so we can in particular ignore the case where both are constant and the switching input is not
+        # furthermore it's impossible for all three to be constant; that would be covered by the base case at the top of the function
+        # so we need only handle the case where either none, or exactly one is constant
+
+        if {![is_const $constants $sigHigh]} {
+            set sigHigh_ab [bdd_mux_abstract $sigHigh [AND $x $high] [AND $x $low] $x_name $constants]
+        } else {
+            set sigHigh_ab [list [list [transitive_simulate $sigHigh] [AND $x $high] [AND $x $low]]]
+        }
+
+        if {![is_const $constants $sigLow]} {
+            set sigLow_ab [bdd_mux_abstract $sigLow [AND [NOT $x] $high] [AND [NOT $x] $low] $x_name $constants]
+        } else {
+            set sigLow_ab [list [list [transitive_simulate $sigLow] [AND [NOT $x] $high] [AND [NOT $x] $low]]]
+        }
+        
         return [list_union [list_union $var_ab $sigHigh_ab] $sigLow_ab]
+
+
     } elseif {$mux_type == "mux_one"} {
         # if one of the inputs is constant, then the mux reduces down to a single AND gate with some inversions
    
@@ -280,7 +310,7 @@ proc bdd_mux_abstract {bdd high low name {constants [list]}} {
         set invert_sw [lindex $invert_list 1]
         set invert_in [lindex $invert_list 2]
 
-        set and_operands [find_big_ands $bdd $invert_out]
+        set and_operands [find_big_ands $bdd $invert_out $constants]
         set and_cases [get_case_exprs [llength $and_operands] $name]
         set and_names [make_unique_names $name [llength $and_operands]]
 
@@ -298,16 +328,15 @@ proc bdd_mux_abstract {bdd high low name {constants [list]}} {
         }
 
 
-        # TODO add the names optimisation hereS
         set result [list]
         foreach op $and_operands case $and_cases x_name $and_names {
             set next_sig [lindex $op 0]
             set invert [lindex $op 1]
 
             if {$invert} {
-                set result [list_union $result [bdd_abstract $next_sig [AND $low_temp $case] $high_temp $x_name]]
+                set result [list_union $result [bdd_abstract $next_sig [AND $low_temp $case] $high_temp $x_name $constants]]
             } else {
-                set result [list_union $result [bdd_abstract $next_sig $high_temp [AND $low_temp $case] $x_name]]
+                set result [list_union $result [bdd_abstract $next_sig $high_temp [AND $low_temp $case] $x_name $constants]]
             }
         }
 
@@ -321,9 +350,10 @@ proc bdd_mux_abstract {bdd high low name {constants [list]}} {
 # for now just convert the signal to a bdd and use the above mux_abstract
 # but this shall have more in it when / if we want to implement non-combinatorial components
 # returns an _abstraction list_ of triples (node, high, low)
-proc bdd_abstract {sig high low name {constants [list]}} {
-    puts "abstracting $sig $high $low"
+proc bdd_abstract {sig high low name {constants ""}} {
+    puts "abstracting $sig $high $low // $constants"
     
+
     # quick continue if we somehow get passed a bdd node
     # FIXME can wire names be purely digits? i doubt it but good to check
     if {[string is digit $sig]} {
@@ -331,14 +361,14 @@ proc bdd_abstract {sig high low name {constants [list]}} {
     }
 
     if {[is_subset [freevars $sig] $constants]} {
-        # TODO transitive simulate
-        set t [VAR c_$sig]
+        puts ">>>>>>>>>>>>>>>> $sig <<<<<<<<<<<<<<<<<< $constants"
+        set t [transitive_simulate $sig]
         return [list [list $t $high $low]]
     }
 
 
     if {[is_VAR $sig]} {
-        set t [VAR v_$sig]
+        set t [VAR $sig]
         return [list [list $t $high $low]]
     }
 
@@ -348,7 +378,10 @@ proc bdd_abstract {sig high low name {constants [list]}} {
 }
 
 # main abstraction entry point
-proc autoabstract {sig high low {constants [list]}} {
+proc autoabstract {sig high low {constants ""}} {
+
+    # find the total area of the circuit covered by constants
+    set constants [forward_prop $constants]
     
     # force any symbolic constants to appear as first in any BDDs
     # this means we cannot have situations where a MUX gate has a constant on a signalling wire but not a switching one
@@ -360,6 +393,7 @@ proc autoabstract {sig high low {constants [list]}} {
         # the current ordering will be maintained, which is contrary to our goal of ensuring our constants are first
         check_symsim -var_order -set [list H2 H1]
         check_symsim -var_order -set $constants
+        puts [check_symsim -var_order -get]
     }
 
     return [bdd_abstract $sig $high $low x $constants]
